9ba9a9c74567c95e1ac73341a9101e4eef4e6a3f
Pedro Neto <pedromneto97@gmail.com>
2017-10-31 14:38:08 -0200

    Rotas

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -4,11 +4,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {InicialComponent} from "./inicial/inicial.component";
 import {AdminComponent} from "./admin.component";
 import {ImoveisComponent} from "./imoveis/imoveis.component";
+import {NovoimovelComponent} from "./novoimovel/novoimovel.component";
 
 const routes: Routes = [{
   path: '', component: AdminComponent, children: [
     {path: '', component: InicialComponent},
     {path: 'imoveis', component: ImoveisComponent},
+    {path: 'novoimovel', component: NovoimovelComponent}
   ]
 }];
 
