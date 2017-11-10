57ba3e13b0a004611885d2d9c86e1f269750dbda
Pedro Neto <pedromneto97@gmail.com>
2017-11-10 14:04:17 -0200

    Formulário funcionando

--- src/app/pages/admin/novoimovel/novoimovel.component.ts
+++ src/app/pages/admin/novoimovel/novoimovel.component.ts
@@ -7,7 +7,7 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./novoimovel.component.css']
 })
 export class NovoimovelComponent implements OnInit {
-  novoImovelForm: FormGroup;
+  ImovelForm: FormGroup;
 
 
   constructor(private formBuilder: FormBuilder) {
@@ -15,30 +15,30 @@ export class NovoimovelComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.novoImovelForm = this.formBuilder.group({
-      numero: [null, Validators.compose([Validators.min(0), Validators.pattern(new RegExp('[0-9]'))])],
-      valor: [null, Validators.compose([Validators.min(100), Validators.pattern(new RegExp('[0-9]'))])],
-      dormitorio: [null, Validators.compose([Validators.pattern(new RegExp('[0-9]'))])],
-      suite: [null, Validators.compose([Validators.pattern(new RegExp('[0-9]'))])],
-      banheiros: [null, Validators.compose([Validators.pattern(new RegExp('[0-9]'))])],
-      garagem: [null, Validators.compose([Validators.pattern(new RegExp('[0-9]'))])],
+    this.ImovelForm = this.formBuilder.group({
+      numero: [null, Validators.compose([Validators.min(0)])],
+      valor: [null, Validators.compose([Validators.required, Validators.min(100)])],
+      dormitorio: [null, Validators.compose([Validators.required, Validators.min(0)])],
+      suite: [null, Validators.compose([Validators.required, Validators.min(0)])],
+      banheiro: [null, Validators.compose([Validators.required, Validators.min(0)])],
+      garagem: [null, Validators.compose([Validators.required, Validators.min(0)])],
       descricao: [null, Validators.compose([Validators.required, Validators.minLength(10)])],
-      aterreno: [null, Validators.compose([Validators.min(0), Validators.pattern(new RegExp('[0-9]'))])],
-      aconstruida: [null, Validators.compose([Validators.min(0), Validators.pattern(new RegExp('[0-9]'))])],
-      rua: this.formBuilder.group({
-        cep: [null, Validators.compose([Validators.required, Validators.pattern(new RegExp('[0-9]{8}'))])],
-        rua: [null, Validators.compose([Validators.minLength(3)])],
-        bairro: this.formBuilder.group({
-          bairro: [null, Validators.compose([Validators.minLength(3)])],
-          cidade: this.formBuilder.group({
-            cidade: [null, Validators.minLength(3)],
-            estado: this.formBuilder.group({
-              estado: [null, Validators.compose([Validators.minLength(3)])],
-              uf: [null, Validators.compose([Validators.min(2), Validators.max(2)])]
-            })
-          })
-        }),
-      }),
+      aterreno: [null, Validators.compose([Validators.required, Validators.min(0)])],
+      aconstruida: [null, Validators.compose([Validators.required, Validators.min(0)])],
+      // rua: this.formBuilder.group({
+      //   cep: [null, Validators.compose([Validators.required, Validators.pattern(new RegExp('[0-9]{8}'))])],
+      //   rua: [null, Validators.compose([Validators.required, Validators.minLength(3)])],
+      //   bairro: this.formBuilder.group({
+      //     bairro: [null, Validators.compose([Validators.required, Validators.minLength(3)])],
+      //     cidade: this.formBuilder.group({
+      //       cidade: [null, Validators.compose([Validators.required, Validators.minLength(3)])],
+      //       estado: this.formBuilder.group({
+      //         estado: [null, Validators.compose([Validators.required, Validators.minLength(3)])],
+      //         uf: [null, Validators.compose([Validators.required, Validators.min(2), Validators.max(2)])]
+      //       })
+      //     })
+      //   }),
+      // }),
     })
     ;
 
@@ -49,7 +49,7 @@ export class NovoimovelComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.novoImovelForm.value);
+    console.log(this.ImovelForm.value);
   }
 
 }
