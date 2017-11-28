702c561bdf3128a03f688f574c049d7332b3c3ad
Pedro Neto <pedromneto97@gmail.com>
2017-11-28 16:15:03 -0200

    Página de login funcionando

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -4,15 +4,22 @@ import {CommonModule} from '@angular/common';
 import {AuthRoutingModule} from './auth-routing.module';
 import {LoginComponent} from "./login/login.component";
 import {AuthComponent} from "./auth.component";
+import {AuthService} from "../../services/auth.service";
+import {FormBuilder, FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   declarations: [
     AuthComponent,
     LoginComponent
+  ],
+  providers: [
+    AuthService
   ]
 })
 export class AuthModule {
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  LoginForm: FormGroup;
+  submit = false;
+
+
+  constructor(private formbuilder: FormBuilder, private auth: AuthService, private router: Router) {
+    this.formbuilder = new FormBuilder();
+  }
 
   ngOnInit() {
+    this.LoginForm = this.formbuilder.group({
+      username: [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+      password: [null, Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(16)])]
+    })
+    ;
+  }
+
+  onSubmit() {
+    this.auth
+      .login(this.LoginForm.value)
+      .then((response) => {
+        localStorage.setItem('API_TOKEN', response.token);
+        this.submit = true;
+        this.router.navigate(['/admin']);
+      });
   }
 
 }
