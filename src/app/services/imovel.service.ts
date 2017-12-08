108985ac92f0ab66e04aae6ba524e91b0cfb412f
Pedro Neto <pedromneto97@gmail.com>
2017-12-08 12:01:20 -0200

    Imovel all

--- src/app/services/imovel.service.ts
+++ src/app/services/imovel.service.ts
@@ -24,7 +24,12 @@ export class ImovelService {
       .then((response: HttpResponse<Imovel>) => response)
       .catch(this.handleError);
   }
-
+  getImoveisLimite() {
+    return this.http.get(this.url, {headers: this.header})
+      .toPromise()
+      .then((response: HttpResponse<Imovel>) => response)
+      .catch(this.handleError);
+  }
   getImovel(id: number) {
     const url = `${this.url}/${id}`;
     return this.http.get(url, {headers: this.header})
