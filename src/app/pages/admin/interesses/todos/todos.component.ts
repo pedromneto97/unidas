68f7e986512c8f33d330b41eec58a16a82c924ed
Pedro Neto <pedromneto97@gmail.com>
2017-12-20 11:33:28 -0200

    Grupo de página de interesses

---
+++ src/app/pages/admin/interesses/todos/todos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosInteresseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
