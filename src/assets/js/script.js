623f797c65f050a90a987330c30c41b94f4bed36
Pedro Neto <pedromneto97@gmail.com>
2017-12-20 15:59:33 -0200

    Konami code

---
+++ src/assets/js/script.js
@@ -0,0 +1,32 @@
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+var konamiCodePosition = 0;
+
+document.addEventListener('keydown', function (e) {
+  var key = allowedKeys[e.keyCode];
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  if (key == requiredKey) {
+    konamiCodePosition++;
+    if (konamiCodePosition == konamiCode.length)
+      activateCheats();
+  } else
+    konamiCodePosition = 0;
+});
+
+function activateCheats() {
+  var doABarrelRoll = function () {
+    var a = "-webkit-", b = 'transform:rotate(1turn);', c = 'transition:4s;';
+    document.head.innerHTML += '<style>body{' + a + b + a + c + b + c
+  }
+  doABarrelRoll();
+}
