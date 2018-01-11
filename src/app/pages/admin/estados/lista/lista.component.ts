3c0c9c1f7907bcf599011cf18bf41068f837d495
Pedro <pedromneto97@gmail.com>
2018-01-11 13:18:28 -0200

    Listagem dos estados, com busca na lista

--- src/app/pages/admin/estados/lista/lista.component.ts
+++ src/app/pages/admin/estados/lista/lista.component.ts
@@ -1,16 +1,49 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
+import {Estado} from '../../../../model/estado';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
   styleUrls: ['./lista.component.css']
 })
-export class ListaEstadoComponent implements OnInit {
+export class ListaEstadoComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  public estados: Estado[];
+  public lista: Estado[];
+  private inscricao: Subscription;
+
+  constructor(private rota: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.inscricao = this.rota.data.subscribe((data: { estado: Estado[] }) => {
+      this.estados = data.estado;
+      this.lista = this.estados;
+    });
+  }
+
+  busca(busca) {
+    if (busca == null || busca === '') {
+      this.lista = this.estados;
+      return;
+    }
+    this.lista = [];
+    const aux = new RegExp(busca, 'i');
+    this.estados.forEach(e => {
+      if (!e.estado.search(aux)) {
+        this.lista.push(e);
+      }
+    });
+  }
+
+  apagar(id) {
+    console.log(id);
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
