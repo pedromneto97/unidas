ffd982e94f6a7c3eed2ae462582f1c52a7c64d51
Pedro <pedromneto97@gmail.com>
2018-01-11 13:16:53 -0200

    Listagem das cidades, carregamento lento

--- src/app/pages/admin/cidades/lista/lista.component.ts
+++ src/app/pages/admin/cidades/lista/lista.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
+import {Cidade} from '../../../../model/cidade';
 
 @Component({
   selector: 'app-lista',
@@ -7,10 +10,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ListaCidadeComponent implements OnInit {
 
-  constructor() {
+  public cidades: Cidade[];
+  private inscricao: Subscription;
+
+  constructor(private rota: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.inscricao = this.rota.data.subscribe((data: { cidade: Cidade[] }) => {
+      this.cidades = data.cidade;
+    });
+  }
+
+  apagar(id) {
+    console.log(id);
   }
 
 }
