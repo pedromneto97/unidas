5d25773d18014eab42f81a19c83148bc9b5219a6
Pedro <pedromneto97@gmail.com>
2018-01-11 13:16:14 -0200

    Listagem dos bairros

--- src/app/pages/admin/bairros/lista/lista.component.ts
+++ src/app/pages/admin/bairros/lista/lista.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Bairro} from '../../../../model/bairro';
+import {Subscription} from 'rxjs/Subscription';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-lista',
@@ -7,10 +10,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ListaBairroComponent implements OnInit {
 
-  constructor() {
+  public bairros: Bairro[];
+  public inscricao: Subscription;
+
+  constructor(private rota: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.inscricao = this.rota.data.subscribe((data: { bairro: Bairro[] }) => {
+      this.bairros = data.bairro;
+    });
+  }
+
+  apagar(id) {
+    console.log(id);
   }
 
 }
