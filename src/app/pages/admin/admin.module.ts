44547d2512660cabe96cf00e9e65448edb3b264b
Pedro <pedromneto97@gmail.com>
2018-01-12 11:01:57 -0200

    Importação e Provider da paginação

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -1,59 +1,61 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 import {AdminRoutingModule} from './admin-routing.module';
 import {InicialComponent} from './inicial/inicial.component';
-import {AdminComponent} from "./admin.component";
-import {ListaImovelComponent} from "./imoveis/lista/lista.component";
+import {AdminComponent} from './admin.component';
+import {ListaImovelComponent} from './imoveis/lista/lista.component';
 import {NovoImovelComponent} from './imoveis/novo/novo.component';
 import {EditarImovelComponent} from './imoveis/editar/editar.component';
 
-import {ImovelService} from "../../services/imovel.service";
-import {RuaService} from "../../services/rua.service";
-import {CidadeService} from "../../services/cidade.service";
-import {EstadoService} from "../../services/estado.service";
-import {BairroService} from "../../services/bairro.service";
+import {ImovelService} from '../../services/imovel.service';
+import {RuaService} from '../../services/rua.service';
+import {CidadeService} from '../../services/cidade.service';
+import {EstadoService} from '../../services/estado.service';
+import {BairroService} from '../../services/bairro.service';
 
-import {FormCanDeactivateGuard} from "../../guard/form-can-deactivate.guard";
-import {AuthService} from "../../services/auth.service";
+import {FormCanDeactivateGuard} from '../../guard/form-can-deactivate.guard';
+import {AuthService} from '../../services/auth.service';
 
 import {EditarRuaComponent} from './ruas/editar/editar.component';
 import {ListaRuaComponent} from './ruas/lista/lista.component';
-import {RuaResolver} from "../../guard/rua.resolver";
+import {RuaResolver} from '../../guard/rua.resolver';
 
 import {ListaBairroComponent} from './bairros/lista/lista.component';
 import {EditarBairroComponent} from './bairros/editar/editar.component';
-import {BairroResolver} from "../../guard/bairro.resolver";
+import {BairroResolver} from '../../guard/bairro.resolver';
 
-import {ListaCidadeComponent} from "./cidades/lista/lista.component";
-import {EditarCidadeComponent} from "./cidades/editar/editar.component";
-import {CidadeResolver} from "../../guard/cidade.resolver";
+import {ListaCidadeComponent} from './cidades/lista/lista.component';
+import {EditarCidadeComponent} from './cidades/editar/editar.component';
+import {CidadeResolver} from '../../guard/cidade.resolver';
 
-import {ListaEstadoComponent} from "./estados/lista/lista.component";
-import {EditarEstadoComponent} from "./estados/editar/editar.component";
-import {EstadoResolver} from "../../guard/estado.resolver";
+import {ListaEstadoComponent} from './estados/lista/lista.component';
+import {EditarEstadoComponent} from './estados/editar/editar.component';
+import {EstadoResolver} from '../../guard/estado.resolver';
 
 import {ListaTipoComponent} from './tipos/lista/lista.component';
 import {NovoTipoComponent} from './tipos/novo/novo.component';
 import {EditarTipoComponent} from './tipos/editar/editar.component';
 
-import {ListaFinalidadeComponent} from "./finalidades/lista/lista.component";
-import {EditarFinalidadeComponent} from "./finalidades/editar/editar.component";
-import {NovoFinalidadeComponent} from "./finalidades/novo/novo.component";
+import {ListaFinalidadeComponent} from './finalidades/lista/lista.component';
+import {EditarFinalidadeComponent} from './finalidades/editar/editar.component';
+import {NovoFinalidadeComponent} from './finalidades/novo/novo.component';
 
 import {ListaInteresseComponent} from './interesses/lista/lista.component';
 import {TodosInteresseComponent} from './interesses/todos/todos.component';
 import {InteresseComponent} from './interesses/interesse/interesse.component';
-import {InteresseResolver} from "../../guard/interesse.resolver";
+import {InteresseResolver} from '../../guard/interesse.resolver';
+import {NgxPaginationModule, PaginatePipe} from 'ngx-pagination';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgxPaginationModule
   ],
   declarations: [
     AdminComponent,
@@ -92,7 +94,8 @@ import {InteresseResolver} from "../../guard/interesse.resolver";
     BairroResolver,
     CidadeResolver,
     EstadoResolver,
-    InteresseResolver
+    InteresseResolver,
+    PaginatePipe
   ]
 })
 export class AdminModule {
