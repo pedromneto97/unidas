0a09acde034f3d531a0ac4054fd981960e10906d
Pedro <pedromneto97@gmail.com>
2018-01-12 13:02:22 -0200

    Implementação da lista, com busca e paginação

--- src/app/pages/admin/finalidades/lista/lista.component.ts
+++ src/app/pages/admin/finalidades/lista/lista.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
+import {Finalidade} from '../../../../model/finalidade';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
   styleUrls: ['./lista.component.css']
 })
-export class ListaFinalidadeComponent implements OnInit {
+export class ListaFinalidadeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public finalidades: Finalidade[];
+  public lista: Finalidade[];
+  public p = 1;
+  private inscricao: Subscription;
+
+  constructor(private rota: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.inscricao = this.rota.data.subscribe((data: { finalidade: Finalidade[] }) => {
+      this.finalidades = data.finalidade;
+      this.lista = data.finalidade;
+      console.log(this.lista);
+    });
+  }
+
+  busca(busca) {
+    if (busca == null || busca === '') {
+      this.lista = this.finalidades;
+      return;
+    }
+    this.lista = [];
+    const aux = new RegExp(busca, 'i');
+    this.finalidades.forEach(e => {
+      if (!e.finalidade.search(aux)) {
+        this.lista.push(e);
+      }
+    });
+  }
+
+  apagar(id) {
+    console.log(id);
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
--- src/app/pages/admin/tipos/lista/lista.component.ts
+++ src/app/pages/admin/tipos/lista/lista.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
+import {Tipo} from '../../../../model/tipo';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
   styleUrls: ['./lista.component.css']
 })
-export class ListaTipoComponent implements OnInit {
+export class ListaTipoComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public tipos: Tipo[];
+  public lista: Tipo[];
+  public p = 1;
+  private inscricao: Subscription;
+
+  constructor(private rota: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.inscricao = this.rota.data.subscribe((data: { tipo: Tipo[] }) => {
+      this.tipos = data.tipo;
+      this.lista = this.tipos;
+    });
+  }
+
+  busca(busca) {
+    if (busca == null || busca === '') {
+      this.lista = this.tipos;
+      return;
+    }
+    this.lista = [];
+    const aux = new RegExp(busca, 'i');
+    this.tipos.forEach(e => {
+      if (!e.tipo.search(aux)) {
+        this.lista.push(e);
+      }
+    });
+  }
+
+  apagar(id) {
+    console.log(id);
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
