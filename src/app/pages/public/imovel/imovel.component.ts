129d61b78a06c69a95629c80b4b5f15882595d81
Pedro <pedromneto97@gmail.com>
2018-01-19 13:33:31 -0200

    Correções e adaptação do modal e carrousel, ainda incompleto, faltando do modal de formulário

--- src/app/pages/public/imovel/imovel.component.ts
+++ src/app/pages/public/imovel/imovel.component.ts
@@ -1,12 +1,16 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, TemplateRef} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Subscription} from 'rxjs/Subscription';
 import {Imovel} from '../../../model/imovel';
+import {BsModalRef, BsModalService, CarouselConfig} from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-imovel',
   templateUrl: './imovel.component.html',
-  styleUrls: ['./imovel.component.css']
+  styleUrls: ['./imovel.component.css'],
+  providers: [
+    {provide: CarouselConfig, useValue: {interval: 3000, noPause: false, showIndicators: true}}
+  ]
 })
 
 export class ImovelComponent implements OnInit, OnDestroy {
@@ -15,8 +19,9 @@ export class ImovelComponent implements OnInit, OnDestroy {
   id: number;
   inscricao: Subscription;
   interesse: any;
+  modalref: BsModalRef;
 
-  constructor(private rota: ActivatedRoute) {
+  constructor(private rota: ActivatedRoute, private modalService: BsModalService) {
   }
 
   ngOnInit() {
@@ -34,4 +39,7 @@ export class ImovelComponent implements OnInit, OnDestroy {
     console.log(form);
   }
 
+  onClick(template: TemplateRef<any>) {
+    this.modalref = this.modalService.show(template);
+  }
 }
